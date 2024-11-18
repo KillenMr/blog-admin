56da2ad19946672b4e95f2092f84b591b420ff1e
fengxian.guo <fenngmr@outlook.com>
2024-11-18 14:30:49 +0800

    兼容手机

--- config/defaultSettings.ts
+++ config/defaultSettings.ts
@@ -15,7 +15,7 @@ const Settings: ProLayoutProps & {
   fixedHeader: false,
   fixSiderbar: true,
   colorWeak: false,
-  collapsed: false,
+  // collapsed: false,
   siderWidth: 240,
   title: 'Plant Flower Tissue Cell Marker',
   pwa: true,
@@ -24,14 +24,14 @@ const Settings: ProLayoutProps & {
   token: {
     // 参见ts声明，demo 见文档，通过token 修改样式
     //https://procomponents.ant.design/components/layout#%E9%80%9A%E8%BF%87-token-%E4%BF%AE%E6%94%B9%E6%A0%B7%E5%BC%8F
-    sider: {
-      colorMenuBackground: '#0E42D2', // menu 的背景颜色
-      colorTextMenu: '#E9F3FF',
-      colorBgMenuItemSelected: '#E7F3FF', // 菜单项选中颜色
-      colorTextMenuSelected: '#0E42D2',
-      colorTextMenuActive: '#E7F3FF', // 菜单项 hover 颜色
-      colorTextMenuItemHover: '#E7F3FF',
-    },
+    // sider: {
+    //   colorMenuBackground: '#0E42D2', // menu 的背景颜色
+    //   colorTextMenu: '#E9F3FF',
+    //   colorBgMenuItemSelected: '#E7F3FF', // 菜单项选中颜色
+    //   colorTextMenuSelected: '#0E42D2',
+    //   colorTextMenuActive: '#E7F3FF', // 菜单项 hover 颜色
+    //   colorTextMenuItemHover: '#E7F3FF',
+    // },
   },
 };
 
