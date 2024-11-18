bb20ba23fe7d418d02c17d16f826f8e0e56fefac
fengxian.guo <fenngmr@outlook.com>
2024-11-18 23:04:12 +0800

    开发页面

--- src/pages/userguides/index.tsx
+++ src/pages/userguides/index.tsx
@@ -78,66 +78,19 @@ const Advanced: FC = () => {
     ),
   };
 
-  const onOperationTabChange = (key: string) => {
-    setTabStatus({ ...tabStatus, operationKey: key as 'tab1' });
-  };
-
   return (
     <PageContainer title=" " className={styles.pageHeader}>
       <div className={styles.main}>
         <GridContent>
-          <Card title="作物基因信息" style={{ marginBottom: 24 }} bordered={false}>
-            <Descriptions style={{ marginBottom: 24 }}>
-              <Descriptions.Item label="作物名称">水稻 (Oryza sativa)</Descriptions.Item>
-              <Descriptions.Item label="基因编号">LOC_Os01g01010</Descriptions.Item>
-              <Descriptions.Item label="研究负责人">张三</Descriptions.Item>
-              <Descriptions.Item label="实验室位置">浙江省农业基因研究所</Descriptions.Item>
-              <Descriptions.Item label="样本描述">
-                水稻基因 LOC_Os01g01010 与抗旱性相关，属于 NAC 基因家族。
-              </Descriptions.Item>
-            </Descriptions>
-
-            <Descriptions title="实验信息" style={{ marginBottom: 24 }}>
-              <Descriptions.Item label="实验类型">CRISPR 基因编辑</Descriptions.Item>
-              <Descriptions.Item label="最新更新">2024-10-17</Descriptions.Item>
-              <Descriptions.Item
-                label={
-                  <span>
-                    编辑工具
-                    <Tooltip title="常用的 CRISPR/Cas9 编辑技术">
-                      <InfoCircleOutlined style={{ color: 'rgba(0, 0, 0, 0.43)', marginLeft: 4 }} />
-                    </Tooltip>
-                  </span>
-                }
-              >
-                CRISPR/Cas9
-              </Descriptions.Item>
-            </Descriptions>
-
-            <h4 style={{ marginBottom: 16 }}>多层级信息组</h4>
-            <Card type="inner" title="基因组详情">
-              <Descriptions style={{ marginBottom: 16 }} title="信息组">
-                <Descriptions.Item label="染色体位置">Chr01: 100-200kb</Descriptions.Item>
-                <Descriptions.Item label="表达水平">高</Descriptions.Item>
-                <Descriptions.Item label="功能注释">
-                  该基因编码 NAC 转录因子，在逆境胁迫中发挥关键作用。
-                </Descriptions.Item>
-              </Descriptions>
-              <Divider style={{ margin: '16px 0' }} />
-              <Descriptions title="其他信息" column={1}>
-                <Descriptions.Item label="物种">
-                  Oryza sativa subsp. indica (籼稻)
-                </Descriptions.Item>
-              </Descriptions>
-            </Card>
-          </Card>
-
-          <Card title="近半年样本采集记录" style={{ marginBottom: 24 }} bordered={false}>
-            <Empty />
-          </Card>
-
-          <Card bordered={false} tabList={operationTabList} onTabChange={onOperationTabChange}>
-            {contentList[tabStatus.operationKey] as React.ReactNode}
+          <Card title="Home" style={{ marginBottom: 24 }} bordered={false}>
+            The home page provides a global view of tissue maps of six plants to quickly explore
+            cell markers by clicking on hyperlinks embedded in the web images. The tissue map for
+            different plants can be switched by clicking the picture at the top right. The tissue
+            maps can facilitate quick browse of cell markers for the listed cell types and more
+            detailed cell types can be shown by clicking the tissue icons. Meanwhile, user can
+            select source (All, Experimental, Bulk RNA-seq and seRNA-seq) for marker genes in the
+            top panel. When click a cell type (for example: root cap), it will jump to the
+            corresponding search result of the cell markers related to the cell type.
           </Card>
         </GridContent>
       </div>
