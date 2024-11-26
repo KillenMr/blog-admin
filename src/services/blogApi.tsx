f2613b7270477bf917aaddcbf80bdcfe01a99185
fengxian.guo <fenngmr@outlook.com>
2024-11-26 13:28:43 +0800

    搜索一处理完

--- src/services/blogApi.tsx
+++ src/services/blogApi.tsx
@@ -1,7 +1,7 @@
 import { request } from '@umijs/max';
 
-export async function getOrgList(params: any) {
-  return request('/api/currentUser', {
+export async function getGenesList(params: any) {
+  return request('/api/Genes', {
     method: 'GET',
     params,
   });
